44944c84f6c5f36720a637d28953e7f3cd27db49
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-01 19:58:58 -0500

    added tree map class and test cases

---
+++ gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
@@ -0,0 +1,60 @@
+package rocks.zipcode;
+
+import gettestie.src.main.java.rocks.zipcode.Address;
+import gettestie.src.main.java.rocks.zipcode.Person;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapTest {
+    private Person testPersonOne;
+    private Person testPersonTwo;
+    private Address addressZCW;
+
+    @Before
+    public void setUp() throws Exception {
+        testPersonOne = new Person("Cay", 1993);
+        testPersonTwo = new Person("Trinh", 1990);
+        addressZCW = new Address("N. Orange St", "Wilmington", "19802", "United States");
+    }
+
+    @Test
+    public void testTreeMapPut() {
+        //given
+        Map<Person, Address> testTreeMap = new TreeMap<>();
+        Address expected = addressZCW;
+        //when
+        testTreeMap.put(testPersonOne, addressZCW);
+        Address actual = testTreeMap.get(testPersonOne);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTreehMapRemove() {
+        //given
+        Map<Person, Address> testTreeMap = new TreeMap<>();
+        testTreeMap.put(testPersonOne, addressZCW);
+        //when
+        testTreeMap.remove(testPersonOne);
+        boolean actual = testTreeMap.containsKey(testPersonOne);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void testTreeMapSize() {
+        //given
+        Map<Person, Address> testTreeMap = new TreeMap<>();
+        testTreeMap.put(testPersonOne, addressZCW);
+        testTreeMap.put(testPersonTwo, addressZCW);
+        int expected = 1;
+        //when
+        testTreeMap.remove(testPersonOne);
+        int actual = testTreeMap.size();
+        //then
+        Assert.assertEquals(expected,actual);
+    }
+
+}
--- gettestie/src/test/java/rocks/zipcode/VectorTest.java
+++ gettestie/src/test/java/rocks/zipcode/VectorTest.java
@@ -3,8 +3,6 @@ import gettestie.src.main.java.rocks.zipcode.Person;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
