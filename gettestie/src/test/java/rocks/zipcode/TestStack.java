9922b7c3a80ec2c8ca623ecc756f595567fe55f4
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-01 20:10:59 -0500

    added some stack test cases, i probably need more

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,12 +1,20 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.Stack;
 
 public class TestStack {
+    Stack<String> stack;
 
     @org.junit.Before
     public void setUp() throws Exception {
+        stack = new Stack<>();
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
     }
 
     @org.junit.After
@@ -20,5 +28,37 @@ public class TestStack {
         assertEquals(false, stack.isEmpty()); // false
     }
 
-    // Make a bigger test exercising more Stack methods.....
+    @Test
+    public void TestStackPop(){
+        //given
+        String expected = "A";
+        //when
+        String actual = stack.pop();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestStackPeek(){
+        //given
+        stack.push("D");
+        String expected = "D";
+        //when
+        String actual = stack.peek();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestStackClear(){
+        //given
+        stack.push("E");
+        Integer expected = 0;
+        //when
+        stack.clear();
+        Integer actual = stack.size();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
